1ecd412d16df6466cce0d9463b5fc0dfb917ef6b
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-29 23:56:26 +1000

    Using react-shapes I'm not able display an actual shape that is sorted to the screen - there is however a big flaw with the react-shapes library which is it only takes two sides for trangles and 2 sides for 'rectangles/squares'.. I would say this is the absoulte minimum mvp

--- src/components/ShapeListItem.js
+++ src/components/ShapeListItem.js
@@ -1,14 +1,19 @@
-import React from "react";
-// import { Link } from "react-router-dom";
+// import React from "react";
+// import { Triangle } from "react-shapes";
 
-const ShapeListItem = ({ sideOne, sideTwo, sideThree, sideFour }) => {
-  return (
-    <div>
-      <p>
-        {sideOne} - {sideTwo} - {sideThree} - {sideFour}
-      </p>
-    </div>
-  );
-};
+// const ShapeListItem = ({ sideOne, sideTwo, sideThree, sideFour }) => {
+//   return (
+//     <div>
+//       <p>
+//         {sideOne} - {sideTwo} - {sideThree} - {sideFour}
+//         <Triangle
+//           width={sideOne}
+//           height={sideTwo}
+//           fill={{ color: "#2409ba" }}
+//         />
+//       </p>
+//     </div>
+//   );
+// };
 
-export default ShapeListItem;
+// export default ShapeListItem;
--- src/components/ShapesList.js
+++ src/components/ShapesList.js
@@ -1,20 +1,54 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import ShapeListItem from "./ShapeListItem";
+// import ShapeListItem from "./ShapeListItem";
 import sortShapes from "../selectors/shapes";
 
-const ShapeList = (props) => {
-  console.log("shapes list =>", props.shapes);
+import { Circle, Triangle, Rectangle } from "react-shapes";
 
+const ShapeListItem = ({ sideOne, sideTwo, sideThree, sideFour }) => {
+  return (
+    <div>
+      <p>
+        {sideOne && !sideTwo && (
+          <Circle
+            r={sideOne * 10}
+            fill={{ color: "#2409ba" }}
+            stroke={{ color: "#E65243" }}
+            strokeWidth={3}
+          />
+        )}
+        {sideTwo && sideTwo && sideThree && !sideFour && (
+          <Triangle
+            width={sideOne * 10}
+            height={sideTwo * 10}
+            fill={{ color: "#2409ba" }}
+            stroke={{ color: "#E65243" }}
+            strokeWidth={3}
+          />
+        )}
+        {sideTwo && sideTwo && sideThree && sideFour && (
+          <Rectangle
+            width={sideOne * 10}
+            height={sideTwo * 10}
+            fill={{ color: "#2409ba" }}
+            stroke={{ color: "#E65243" }}
+            strokeWidth={3}
+          />
+        )}
+      </p>
+    </div>
+  );
+};
+
+const ShapeList = (props) => {
   return (
     <div>
       <>
         <h1>Sorted Shapes</h1>
         {props.shapes.map((shape) => {
-          console.log("this is a shape", shape);
           return <ShapeListItem {...shape} />;
-        })}{" "}
+        })}
       </>
     </div>
   );
