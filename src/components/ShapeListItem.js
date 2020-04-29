8e0a34ac92c4484b62e1a073eaa6f269b03736ee
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-30 00:00:31 +1000

    moved ShapeListItem back to it's own file

--- src/components/ShapeListItem.js
+++ src/components/ShapeListItem.js
@@ -1,19 +1,39 @@
-// import React from "react";
-// import { Triangle } from "react-shapes";
+import React from "react";
+import { Circle, Triangle, Rectangle } from "react-shapes";
 
-// const ShapeListItem = ({ sideOne, sideTwo, sideThree, sideFour }) => {
-//   return (
-//     <div>
-//       <p>
-//         {sideOne} - {sideTwo} - {sideThree} - {sideFour}
-//         <Triangle
-//           width={sideOne}
-//           height={sideTwo}
-//           fill={{ color: "#2409ba" }}
-//         />
-//       </p>
-//     </div>
-//   );
-// };
+const ShapeListItem = ({ sideOne, sideTwo, sideThree, sideFour }) => {
+  return (
+    <div>
+      <p>
+        {sideOne && !sideTwo && (
+          <Circle
+            r={sideOne * 10}
+            fill={{ color: "#2409ba" }}
+            stroke={{ color: "#E65243" }}
+            strokeWidth={3}
+          />
+        )}
+        {sideTwo && sideTwo && sideThree && !sideFour && (
+          <Triangle
+            width={sideOne * 10}
+            height={sideTwo * 10}
+            fill={{ color: "#2409ba" }}
+            stroke={{ color: "#E65243" }}
+            strokeWidth={3}
+          />
+        )}
+        {sideTwo && sideTwo && sideThree && sideFour && (
+          <Rectangle
+            width={sideOne * 10}
+            height={sideTwo * 10}
+            fill={{ color: "#2409ba" }}
+            stroke={{ color: "#E65243" }}
+            strokeWidth={3}
+          />
+        )}
+      </p>
+    </div>
+  );
+};
 
-// export default ShapeListItem;
+export default ShapeListItem;
--- src/components/ShapesList.js
+++ src/components/ShapesList.js
@@ -1,45 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 
-// import ShapeListItem from "./ShapeListItem";
+import ShapeListItem from "./ShapeListItem";
 import sortShapes from "../selectors/shapes";
-
-import { Circle, Triangle, Rectangle } from "react-shapes";
-
-const ShapeListItem = ({ sideOne, sideTwo, sideThree, sideFour }) => {
-  return (
-    <div>
-      <p>
-        {sideOne && !sideTwo && (
-          <Circle
-            r={sideOne * 10}
-            fill={{ color: "#2409ba" }}
-            stroke={{ color: "#E65243" }}
-            strokeWidth={3}
-          />
-        )}
-        {sideTwo && sideTwo && sideThree && !sideFour && (
-          <Triangle
-            width={sideOne * 10}
-            height={sideTwo * 10}
-            fill={{ color: "#2409ba" }}
-            stroke={{ color: "#E65243" }}
-            strokeWidth={3}
-          />
-        )}
-        {sideTwo && sideTwo && sideThree && sideFour && (
-          <Rectangle
-            width={sideOne * 10}
-            height={sideTwo * 10}
-            fill={{ color: "#2409ba" }}
-            stroke={{ color: "#E65243" }}
-            strokeWidth={3}
-          />
-        )}
-      </p>
-    </div>
-  );
-};
 
 const ShapeList = (props) => {
   return (
