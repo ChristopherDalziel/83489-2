17ed78553661af1b63efa19386a0453f33a7aa65
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-29 23:10:27 +1000

    Removed extra ShapeListItem from displaying empty all the time

--- src/components/ShapesList.js
+++ src/components/ShapesList.js
@@ -7,16 +7,18 @@ import sortShapes from "../selectors/shapes";
 const ShapeList = (props) => {
   console.log("shapes list =>", props.shapes);
 
-  return <div>ok</div>;
+  return (
+    <div>
+      <>
+        <h1>Sorted Shapes</h1>
+        {props.shapes.map((shape) => {
+          console.log("this is a shape", shape);
+          return <ShapeListItem {...shape} />;
+        })}{" "}
+      </>
+    </div>
+  );
 };
-// <div>
-//   <h1>Sorted Shapes</h1>
-//   <ShapeListItem />
-//   {props.shapes.map((shape) => {
-//     console.log("this is a shape", shape);
-//     return <ShapeListItem {...shape} />;
-//   })}
-// </div>
 
 const mapStateToProps = (state) => {
   return {
