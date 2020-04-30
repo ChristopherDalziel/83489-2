afee31308783dee7bc2fd482c3bed5ee61006c06
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-30 21:58:56 +1000

    Added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
 import styled from "@emotion/styled";
+import logo from "../assets/logo.png";
 
 const HeaderDiv = styled.div`
   padding: 0;
   margin: 0;
   background-color: #e63846;
   height: 10vh;
+  border-radius: 0px 0px 100px 0px;
+  box-shadow: 17px 14px 17px -11px rgba(0, 0, 0, 0.2);
 `;
 
 const H1 = styled.h1`
   margin: 0;
   padding: 0px 10px 0px 10px;
+  width: 30vw;
 `;
 
 const H3 = styled.h3`
@@ -18,18 +22,27 @@ const H3 = styled.h3`
   padding: 0px 10px 0px 10px;
 `;
 
+const LogoSmall = styled.img`
+  height: 75px;
+  float: right;
+`;
+
 const Header = (props) => {
   return (
     <HeaderDiv>
-      <H1>{props.title}</H1>
+      <H1>
+        {props.title}
+        <LogoSmall src={logo} />
+      </H1>
+
       {props.subTitle && <H3>{props.subTitle}</H3>}
     </HeaderDiv>
   );
 };
 
 Header.defaultProps = {
-  title: "Unnamed Application",
-  subTitle: "Let us sort your shapes",
+  title: "Shape Sorter Online",
+  subTitle: "Leave your shape sorting to a computer",
 };
 
 export default Header;
