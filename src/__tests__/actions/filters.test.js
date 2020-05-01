0b3c2e52f1df2005274be99d750946861ca853c5
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-05-01 17:04:05 +1000

    Added tests for filters actions

---
+++ src/__tests__/actions/filters.test.js
@@ -0,0 +1,32 @@
+import {
+  sortByTriangles,
+  sortByCircles,
+  sortByQuadrilaterals,
+} from "../../actions/filters";
+
+test("Should generate sort by triangles action object", () => {
+  const sortBy = "Triangles";
+  const action = sortByTriangles(sortBy);
+  expect(action).toEqual({
+    type: "SORT_BY_TRIANGLES",
+    sortBy,
+  });
+});
+
+test("Should generate sort by circles action object", () => {
+  const sortBy = "Circles";
+  const action = sortByCircles(sortBy);
+  expect(action).toEqual({
+    type: "SORT_BY_CIRCLES",
+    sortBy,
+  });
+});
+
+test("Should generate sort by triangles action object", () => {
+  const sortBy = "Quadrilaterals";
+  const action = sortByQuadrilaterals(sortBy);
+  expect(action).toEqual({
+    type: "SORT_BY_QUADRILATERALS",
+    sortBy,
+  });
+});
