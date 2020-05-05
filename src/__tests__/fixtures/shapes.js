3a3dfa45c6798ffc8b407dbee374edf1adff4a86
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-05-05 21:00:52 +1000

    Added shape fixture file, allowing me to import my test shapes without having to re-write code for no reason

---
+++ src/__tests__/fixtures/shapes.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    sideOne: 10,
+  },
+  { sideOne: 5, sideTwo: 5, sideThree: 5 },
+  { sideOne: 5, sideTwo: 10, sideThree: 5, sideFour: 10 },
+];
--- src/__tests__/selectors/shapes.test.js
+++ src/__tests__/selectors/shapes.test.js
@@ -1,12 +1,5 @@
 import sortShapes from "../../selectors/shapes";
-
-const shapes = [
-  {
-    sideOne: 10,
-  },
-  { sideOne: 5, sideTwo: 5, sideThree: 5 },
-  { sideOne: 5, sideTwo: 10, sideThree: 5, sideFour: 10 },
-];
+import shapes from "../fixtures/shapes";
 
 test("Should filter shapes with only one side and return them as Circles", () => {
   const filters = {
