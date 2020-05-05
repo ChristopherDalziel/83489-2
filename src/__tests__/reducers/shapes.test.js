6504755c7058174bca931c6c8eed16d8abbec355
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-05-05 21:31:02 +1000

    Created shape reducers tests

---
+++ src/__tests__/reducers/shapes.test.js
@@ -0,0 +1,22 @@
+import shapesReducer from "../../reducers/shapes";
+import shapes from "../fixtures/shapes";
+
+test("Should set default state", () => {
+  const state = shapesReducer(undefined, { type: "@@INIT" });
+  expect(state).toEqual([]);
+});
+
+test("Should add a new shape", () => {
+  const shape = {
+    sideOne: 25,
+    sideTwo: 45,
+    sideThree: 10,
+    sideFour: 33,
+  };
+  const action = {
+    type: "ADD_SHAPE",
+    shape,
+  };
+  const state = shapesReducer(shapes, action);
+  expect(state).toEqual([...shapes, shape]);
+});
