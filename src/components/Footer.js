33470bb190282c9f5bd8b7156d18eb206b7777e5
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-05-06 23:02:55 +1000

    Added links to footer, and font awesome icons, added basic css to prevent ugly colours of previously clicked links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,33 @@ import styled from "@emotion/styled";
 
 const FooterDiv = styled.footer`
   margin-top: auto;
-  height: 8vh;
+  height: 5vh;
   background-color: #e63846;
   border-radius: 100px 0px 0px 0px;
   display: flex;
   align-items: flex-end;
+  justify-content: flex-end;
   padding: 2vh;
-  float: right;
+  font-size: 50px;
 `;
 
 const Footer = () => {
-  return <FooterDiv>Christopher Dalziel</FooterDiv>;
+  return (
+    <FooterDiv>
+      <a
+        target="_blank"
+        href="https://github.com/ChristopherDalziel/83489-2"
+        class="fab fa-github-square socials"
+        id="footerLink"
+      ></a>
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/in/christopher-dalziel-129427187/"
+        class="fab fa-linkedin socials"
+        id="footerLink"
+      ></a>
+    </FooterDiv>
+  );
 };
 
 export default Footer;
