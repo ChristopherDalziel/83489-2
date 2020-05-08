c27d86e0ba9de5ff7a8afd2bd81f40ef061210dd
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-05-09 01:14:17 +1000

    Added test for ShapeListItem, currently displays incorrect snapshot as it's just showing there is a styled div there but no further information

--- src/__tests__/components/ShapeListItem.test.js
+++ src/__tests__/components/ShapeListItem.test.js
@@ -1,4 +1,21 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { ShapeList } from "../../components/ShapeList";
+import ShapeListItem from "../../components/ShapeListItem";
+import { Quadrilateral } from "../../components/ShapeListItem";
 import shapes from "../fixtures/shapes";
+
+import toJSON from "enzyme-to-json";
+
+test("Should render shape list item with data", () => {
+  const wrapper = shallow(
+    <ShapeListItem
+      sideOne={shapes[0].sideOne}
+      sideTwo={shapes[0].sideTwo}
+      sideThree={shapes[0].sideThree}
+      sideFour={shapes[0].sideFour}
+    />
+  );
+  expect(toJSON(wrapper)).toMatchSnapshot();
+});
+
+// Wrapper returns weird due to returning styled div's or konva. Will come back to this.
