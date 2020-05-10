87233e1729045b70a53ee8dee62793be67b54ae5
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-05-11 00:10:07 +1000

    Removed code comments of previous filters action and reducers

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,18 +1,3 @@
-// export const sortByTriangles = (sortBy = "Triangles") => ({
-//   type: "SORT_BY_TRIANGLES",
-//   sortBy,
-// });
-
-// export const sortByCircles = (sortBy = "Circles") => ({
-//   type: "SORT_BY_CIRCLES",
-//   sortBy,
-// });
-
-// export const sortByQuadrilaterals = (sortBy = "Quadrilaterals") => ({
-//   type: "SORT_BY_QUADRILATERALS",
-//   sortBy,
-// });
-
 const sortBy = (key) => ({
   type: "SORT_BY",
   sortBy: key,
--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -2,28 +2,6 @@ const filterReducerDefaultState = {
   sortBy: "Triangles",
 };
 
-// const filterReducer = (state = filterReducerDefaultState, action) => {
-//   switch (action.type) {
-//     case "SORT_BY_TRIANGLES":
-//       return {
-//         ...state,
-//         sortBy: "Triangles",
-//       };
-//     case "SORT_BY_CIRCLES":
-//       return {
-//         ...state,
-//         sortBy: "Circles",
-//       };
-//     case "SORT_BY_QUADRILATERALS":
-//       return {
-//         ...state,
-//         sortBy: "Quadrilaterals",
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
 const filterReducer = (state = filterReducerDefaultState, action) => {
   switch (action.type) {
     case "SORT_BY":
